90a0576f27f97017f0481dbaab5e29cd31d6ffe6
Davide N <davideneri18@gmail.com>
2025-01-31 11:32:34 +0100

    change morse pause

--- projects/01-morse-tx/main.py
+++ projects/01-morse-tx/main.py
@@ -5,9 +5,16 @@ from modulino import ModulinoBuzzer
 
 buzzer = ModulinoBuzzer()
 
-pause = 200  # pause in millisecond
-dotDuration = 50
-lineDuration = 200
+
+duration = 200 # duration in millisecond
+
+dit = duration
+dah = 3 * duration
+
+pause = duration
+letterPause = 2 * duration
+wordPause = 6 * duration  # in total 7 duration
+
 
 while True:
     print("Insert the message:")
@@ -15,19 +22,20 @@ while True:
     for l in data:
       print(l)
       if l == " ":
-        sleep_ms(7 * pause)
+        sleep_ms(wordPause)
       else:
         if l == "s":
-          buzzer.tone(440, dotDuration, True)
+          buzzer.tone(440, dit, True)
           sleep_ms(pause)
-          buzzer.tone(440, dotDuration, True)
+          buzzer.tone(440, dit, True)
           sleep_ms(pause)
-          buzzer.tone(440, dotDuration, True)
+          buzzer.tone(440, dit, True)
           sleep_ms(pause)
         elif l == "o":
-          buzzer.tone(440, lineDuration, True)
+          buzzer.tone(440, dah, True)
           sleep_ms(pause)
-          buzzer.tone(440, lineDuration, True)
+          buzzer.tone(440, dah, True)
           sleep_ms(pause)
-          buzzer.tone(440, lineDuration, True)
-        sleep_ms(2 * pause)
+          buzzer.tone(440, dah, True)
+          sleep_ms(pause)
+        sleep_ms(letterPause)
